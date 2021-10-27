2ab103f555595b66f74cb03615df0e74b51a4b27
Yegor Zhukovskiy <yegorzhukovskiya@gmail.com>
2021-10-27 20:23:10 +0300

    Added remaining dependencies

--- commands/play.ts
+++ commands/play.ts
@@ -1,11 +1,14 @@
 import { SlashCommandBuilder } from '@discordjs/builders'
 import {CommandInteraction, Interaction, InteractionType} from "discord.js";
 
-module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('ping')
-        .setDescription('Replies with Pong!'),
-    async execute(interaction: CommandInteraction) {
-        await interaction.reply('Pong!');
-    }
-}
+// module.exports = {
+//     data: new SlashCommandBuilder()
+//         .setName('ping')
+//         .setDescription('Replies with Pong!'),
+//     async execute(interaction: CommandInteraction) {
+//         await interaction.reply('Pong!');
+//     }
+// }
+const { generateDependencyReport } = require('@discordjs/voice');
+
+console.log(generateDependencyReport());
