a736b29afa1761406463e987f735ad5c128fc02c
Yegor Zhukovskiy <yegorzhukovskiya@gmail.com>
2021-10-28 00:24:23 +0300

    Implemented basic /play command. Bot can join server and stream audio from given youtube link

--- commands/play.js
+++ commands/play.js
@@ -8,13 +8,18 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const builders_1 = require("@discordjs/builders");
 const voice_1 = require("@discordjs/voice");
+const ytdl_core_1 = __importDefault(require("ytdl-core"));
 module.exports = {
     data: new builders_1.SlashCommandBuilder()
         .setName('play')
-        .setDescription('Plays first found youtube video by keyword or link.'),
+        .setDescription('Plays first found youtube video by keyword or link.')
+        .addStringOption(option => option.setName('link').setDescription('Link duh').setRequired(true)),
     execute(interaction) {
         return __awaiter(this, void 0, void 0, function* () {
             const member = interaction.member;
@@ -24,8 +29,25 @@ module.exports = {
                     guildId: member.voice.channel.guild.id,
                     adapterCreator: member.voice.channel.guild.voiceAdapterCreator,
                 });
+                const player = (0, voice_1.createAudioPlayer)();
+                connection.subscribe(player);
+                player.on('error', error => {
+                    console.error('Error:', error.message);
+                });
+                const link = interaction.options.getString('link');
+                if (typeof link !== "string")
+                    return interaction.reply('Error with given data');
+                const resource = (0, voice_1.createAudioResource)((0, ytdl_core_1.default)(link, { filter: 'audioonly', quality: 'highestaudio' }), {
+                    inputType: voice_1.StreamType.WebmOpus
+                });
+                resource.playStream.on('readable', () => __awaiter(this, void 0, void 0, function* () {
+                    player.play(resource);
+                    yield interaction.reply(`Playing ${link}`);
+                }));
             }
-            yield interaction.reply('Pong!');
+            else {
+                yield interaction.reply({ content: 'You must be in a voice channel.', ephemeral: true });
+            }
         });
     }
 };
--- commands/play.ts
+++ commands/play.ts
@@ -1,23 +1,51 @@
-import { SlashCommandBuilder } from '@discordjs/builders'
-import {CommandInteraction, Guild, GuildMember, Interaction, InteractionType, VoiceState} from "discord.js";
-import {APIInteractionGuildMember} from 'discord-api-types';
-import {DiscordGatewayAdapterCreator, joinVoiceChannel, VoiceConnection} from "@discordjs/voice";
+import {SlashCommandBuilder} from '@discordjs/builders'
+import {CommandInteraction, GuildMember} from "discord.js";
+import {
+    AudioPlayer,
+    AudioResource,
+    createAudioPlayer,
+    createAudioResource,
+    DiscordGatewayAdapterCreator,
+    joinVoiceChannel,
+    StreamType,
+    VoiceConnection
+} from "@discordjs/voice";
+import ytdl from "ytdl-core";
+import fs from "fs";
+import path from "path";
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('play')
-        .setDescription('Plays first found youtube video by keyword or link.'),
+        .setDescription('Plays first found youtube video by keyword or link.')
+        .addStringOption(option => option.setName('link').setDescription('Link duh').setRequired(true)),
     async execute(interaction: CommandInteraction) {
 
-        const member: GuildMember | null = interaction.member as GuildMember;
+        if(!(interaction.member instanceof GuildMember)) return await interaction.reply(`Some dumb error with member type`);
+        const member: GuildMember = interaction.member;
         if(member.voice.channel){
             const connection: VoiceConnection = joinVoiceChannel({
                 channelId: member.voice.channel.id,
                 guildId: member.voice.channel.guild.id,
                 adapterCreator: member.voice.channel.guild.voiceAdapterCreator as DiscordGatewayAdapterCreator,
             });
-        }
 
-        await interaction.reply('Pong!');
+            const player: AudioPlayer  = createAudioPlayer();
+            connection.subscribe(player);
+            player.on('error', error => {
+                console.error('Error:', error.message);
+            });
+
+            const link = interaction.options.getString('link');
+            if(typeof link !== "string") return interaction.reply('Error with given data');
+            const resource: AudioResource = createAudioResource(ytdl(link, { filter: 'audioonly', quality: 'highestaudio'}), {
+                inputType: StreamType.WebmOpus
+            });
+
+             resource.playStream.on('readable', async () =>{
+                player.play(resource);
+                await interaction.reply(`Playing ${link}`);
+            })
+        } else { await interaction.reply({ content: 'You must be in a voice channel.', ephemeral: true });}
     }
 }
