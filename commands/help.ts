2c276b950fe9e360a3b23e6e465a754531c6d1de
YegorZh <43119306+YegorZh@users.noreply.github.com>
2022-03-13 17:34:19 +0200

    Update help.ts
    Small changes to help text.

--- commands/help.ts
+++ commands/help.ts
@@ -8,14 +8,14 @@ module.exports = {
         .setDescription('Lists all commands and their descriptions'),
     async execute(interaction: CommandInteraction, data: GuildMusDataArr) {
         const str =
-            'Play - plays the song from the link replacing current playing track.\n' +
-            'Add - adds song to the playlist.\n'+
-            'Skip - skips current song.\n' +
-            'Loop - loops or stops looping current song.\n' +
-            'LoopQueue - loops entire playlist\n.' +
-            'Pause - pauses playing.\n' +
-            'Resume - resumes playing.\n' +
-            'Stop - makes bot completely stop playing and exit the voice channel.'
+            'Play: plays the song from the link (or first result of search phrase) replacing current playing track.\n' +
+            'Add: adds song to the queue.\n'+
+            'Skip: skips current song.\n' +
+            'Loop: loops or stops looping current song.\n' +
+            'LoopQueue: loops entire queue.\n' +
+            'Pause: pauses playing.\n' +
+            'Resume: resumes playing.\n' +
+            'Stop: makes bot completely stop playing and exit the voice channel.'
         await interaction.reply({content: str, ephemeral: true});
     }
 }
